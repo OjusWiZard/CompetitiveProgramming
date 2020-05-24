437823146f4267ad26060f53a3dda056bcb57efc
OjusWiZard <ojuswimail@gmail.com>
2020-05-24 19:29:46 +0530

    Practice

---
+++ CodeForces/104-A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio( false ) ;
+	cin.tie( NULL ) ;
+
+	int n ;
+	cin>>n ;
+	switch( n )
+	{
+		case 11 :
+			cout<<4 ; break ;
+		case 12 :
+			cout<<4 ; break ;
+		case 13 :
+			cout<<4 ; break ;
+		case 14 :
+			cout<<4 ; break ;
+		case 15 :
+			cout<<4 ; break ;
+		case 16 :
+			cout<<4 ; break ;
+		case 17 :
+			cout<<4 ; break ;
+		case 18 :
+			cout<<4 ; break ;
+		case 19 :
+			cout<<4 ; break ;
+		case 20 :
+			cout<<15 ; break ;
+		case 21 :
+			cout<<4 ; break ;
+		default :
+			cout<<0 ;
+	}
+	return 0 ;
+}
